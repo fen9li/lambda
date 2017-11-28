43b43235a967415a47fe1679e0449502b3a33e56
Feng Li <lifcn@yahoo.com>
2017-11-29 10:43:48 +1100

    Initial Commit.

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# lambda
-aws lambda templates
+# AWS Lambda Use Case Study 01 - with S3
+
+A text file uploaded to S3 bucket, which triggers Lambda function to process this file and save the result text file to another bucket.
+
+## AWS resources / services consumed / used in this project
+* Lambda 
+* 2 s3 buckets
+* CloudWatch logs
+* IAM role
+
+## Usage
+
+### Prepare
+
+> Setup a linux host as management host, which is both aws cli and Github ready. The linux host can be an on-premise one or an aws ec2 instance; Can be a physical one or virtual. 
+
+### Set up
+  
---
+++ append_line.py
@@ -0,0 +1,28 @@
+from __future__ import print_function
+import boto3
+import os
+import sys
+import uuid
+     
+s3_client = boto3.client('s3')
+     
+def append_line(in_path, out_path):
+    with open(in_path) as fin:
+         with open(out_path, "a+") as fout:
+              for line in fin:
+                  fout.write(line)
+
+              fout.write("This line is appended by Lambda function... \n")
+         fout.close()
+    fin.close()
+     
+def append_line_handler(event, context):
+    for record in event['Records']:
+        bucket = record['s3']['bucket']['name']
+        key = record['s3']['object']['key'] 
+        download_path = '/tmp/{}{}'.format(uuid.uuid4(), key)
+        upload_path = '/tmp/processed-{}'.format(key)
+        
+        s3_client.download_file(bucket, key, download_path)
+        append_line(download_path, upload_path)
+        s3_client.upload_file(upload_path, '{}processed'.format(bucket), key)
