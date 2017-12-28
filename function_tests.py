4cde3aec3f6a648e57c4c22b67b076dfabdc5518
Feng Li <lifcn@yahoo.com>
2017-12-29 00:58:13 +1100

    Add deleting s3 bucket feature.

--- function_tests.py
+++ function_tests.py
@@ -13,6 +13,24 @@ class LambdaFunctionTest(unittest.TestCase):
       FNAME = HOME + '/lambda/lambda_conf.py'
       self.assertTrue(os.path.exists(FNAME))
 
+  def test_deleting_aws_s3_bucket(self):
+      # Uli wants to delete his s3 bucket
+      # No matter it is empty or not
+      # His bucket name is defined as 's3_bucket' in 'lambda_conf.py'
+      import lambda_conf
+      import manipulate_s3_bucket
+
+      # Save something in the bucket
+      s3 = boto3.resource('s3')
+      s3.meta.client.upload_file('/home/fli/lambda/README.md',lambda_conf.s3_bucket,'README.md')
+      s3.meta.client.upload_file('/home/fli/lambda/.gitignore',lambda_conf.s3_bucket,'.gitignore')
+
+      manipulate_s3_bucket.delete_bucket(lambda_conf.s3_bucket)
+
+      # Test if the bucket exists.
+      check = s3.Bucket(lambda_conf.s3_bucket) in s3.buckets.all()
+      self.assertFalse(check)
+
       self.fail('Finish the test!')
 
 if __name__ == '__main__':
--- manipulate_s3_bucket.py
+++ manipulate_s3_bucket.py
@@ -20,3 +20,24 @@ def create_bucket(bucket_name,region):
         except Exception as error:
             print(error)
         return bucket
+
+def delete_bucket(bucket_name):
+
+    # Delete the bucket.
+    s3 = boto3.resource('s3')
+    bucket = s3.Bucket(bucket_name)
+
+    # Check if the bucket exists 
+    if(bucket in s3.buckets.all()):
+        try:
+            # Delete any contents in the bucket if any
+            for key in bucket.objects.all():
+                key.delete()
+            # Delete the bucket
+            bucket.delete()
+        except Exception as error:
+            print(error)
+    else:
+        # The bucket doesn't exist
+        print('BucketNotExists')
+    return True
